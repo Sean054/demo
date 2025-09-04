31ab9d9f754f7f9cf87679721e5c2b076778309b
Sean <seanjstovall96r@email.com>
2025-09-04 13:11:56 -0500

    init git repo and test.txt

---
+++ .vscode/project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the wheel
+// 5. Check the results
+// 6. Give the player their winnings
+// 7. Play again
